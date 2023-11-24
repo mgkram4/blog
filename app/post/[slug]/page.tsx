1068cce1d34d3d5c39840dc9214edde31552fee6
Mark <mark.garcia4@laverne.edu>
2023-11-24 10:49:54 -0800

    new test

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -41,7 +41,7 @@ export default async function SlugPage({
         <div className="space-y-1 text-center">
           <div className="space-y-10">
             <div>
-              <p className="text-base font-medium leading-6 text-pink-500">
+              <p className="text-base font-medium leading-6 text-slate-500">
                 {new Date(data._createdAt).toLocaleDateString("en-US", {
                   year: "numeric",
                   month: "long",
