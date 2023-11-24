ae8ff809f98d8656c05a147ae6154bbfe18b9d44
Mark <mark.garcia4@laverne.edu>
2023-11-24 11:13:32 -0800

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Providers } from "./components/Providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Stoic Blog",
+  title: "StayStoic",
   description: "Stoic's Guide to Life",
 };
 
